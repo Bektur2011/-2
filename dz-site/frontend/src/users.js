ac24674b9c55e228acebce749eebf17156040b23
Bektur2011 <b3474850@gmail.com>
2025-12-29 12:40:39 +0600

    Update users.js

--- dz-site/frontend/src/users.js
+++ dz-site/frontend/src/users.js
@@ -1,5 +1,7 @@
 export default [
+  //Админы
   { login: "bb34_14", name: "Бектур", role: "Admin", gender: "male" },
+  { login: "ya12_14", name: "Даткайым", role: "Admin", gender: "female" },//Бекмырзаева
 
   // Мальчики (13)
   { login: "dd67_14", name: "Мырзабек", role: "Student", gender: "male" },
@@ -7,31 +9,30 @@ export default [
   { login: "ar11_14", name: "Алманбет", role: "Student", gender: "male" },
   { login: "il45_14", name: "Биймырза", role: "Student", gender: "male" },
   { login: "da77_14", name: "Балбай", role: "Student", gender: "male" },
-  { login: "ti33_14", name: "", role: "Student", gender: "male" },
-  { login: "ra55_14", name: "", role: "Student", gender: "male" },
-  { login: "ni66_14", name: "", role: "Student", gender: "male" },
-  { login: "eg88_14", name: "", role: "Student", gender: "male" },
-  { login: "ma99_14", name: "", role: "Student", gender: "male" },
-  { login: "ro10_14", name: "", role: "Student", gender: "male" },
-  { login: "vl12_14", name: "", role: "Student", gender: "male" },
-  { login: "ki13_14", name: "", role: "Student", gender: "male" },
+  { login: "ti33_14", name: "Исхак", role: "Student", gender: "male" },
+  { login: "ra55_14", name: "Аман", role: "Student", gender: "male" },
+  { login: "ni66_14", name: "Кубат", role: "Student", gender: "male" },
+  { login: "eg88_14", name: "Элхан", role: "Student", gender: "male" },
+  { login: "ma99_14", name: "Даян", role: "Student", gender: "male" },
+  { login: "ro10_14", name: "Примбай", role: "Student", gender: "male" },
+  { login: "vl12_14", name: "Канат", role: "Student", gender: "male" },
+  { login: "ki13_14", name: "Барсбек", role: "Student", gender: "male" },
 
   // Девочки (17)
-  { login: "ma01_14", name: "", role: "Student", gender: "female" },
-  { login: "an02_14", name: "", role: "Student", gender: "female" },
-  { login: "so03_14", name: "", role: "Student", gender: "female" },
-  { login: "va04_14", name: "", role: "Student", gender: "female" },
-  { login: "da05_14", name: "", role: "Student", gender: "female" },
-  { login: "al06_14", name: "", role: "Student", gender: "female" },
-  { login: "po07_14", name: "", role: "Student", gender: "female" },
-  { login: "el08_14", name: "", role: "Student", gender: "female" },
-  { login: "kr09_14", name: "", role: "Student", gender: "female" },
-  { login: "mi10_14", name: "", role: "Student", gender: "female" },
-  { login: "ve11_14", name: "", role: "Student", gender: "female" },
-  { login: "ya12_14", name: "", role: "Student", gender: "female" },
-  { login: "li13_14", name: "", role: "Student", gender: "female" },
-  { login: "ar14_14", name: "", role: "Student", gender: "female" },
-  { login: "ol15_14", name: "", role: "Student", gender: "female" },
-  { login: "na16_14", name: "", role: "Student", gender: "female" },
+  { login: "ma01_14", name: "Сабина", role: "Student", gender: "female" },
+  { login: "an02_14", name: "Алия", role: "Student", gender: "female" },
+  { login: "so03_14", name: "Аяна", role: "Student", gender: "female" },//Табалдиева
+  { login: "va04_14", name: "Асия", role: "Student", gender: "female" },
+  { login: "da05_14", name: "Айзирек", role: "Student", gender: "female" },
+  { login: "al06_14", name: "Элина", role: "Student", gender: "female" },
+  { login: "po07_14", name: "Азема", role: "Student", gender: "female" },
+  { login: "el08_14", name: "Аяна", role: "Student", gender: "female" },//Адыева
+  { login: "kr09_14", name: "Сумая", role: "Student", gender: "female" },
+  { login: "mi10_14", name: "сезимай", role: "Student", gender: "female" },
+  { login: "ve11_14", name: "Даткайым", role: "Student", gender: "female" },//Жусупаева
+  { login: "li13_14", name: "Айша", role: "Student", gender: "female" },
+  { login: "ar14_14", name: "Нурбийке", role: "Student", gender: "female" },
+  { login: "ol15_14", name: "Айданек", role: "Student", gender: "female" },
+  { login: "na16_14", name: "Диана", role: "Student", gender: "female" },
   { login: "ka17_14", name: "", role: "Student", gender: "female" }
 ];
